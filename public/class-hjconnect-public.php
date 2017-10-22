6ad197803e13aee66e476b9d634f6741121df8e2
Leighton <leighsapir@icloud.com>
2017-10-22 16:55:22 -0500

    [Trash] Remove unnecessary code

--- public/class-hjconnect-public.php
+++ public/class-hjconnect-public.php
@@ -56,52 +56,6 @@ class Hjconnect_Public {
 
 	}
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Hjconnect_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Hjconnect_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/hjconnect-public.css', array(), $this->version, 'all' );
-
-	}
-
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Hjconnect_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Hjconnect_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/hjconnect-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
 	public function hotjar_connect() {
 		
 		if ( ! empty( $this->hjconnect_options['hotjar_on'] ) && ! empty( $this->hjconnect_options['hotjar_id'] ) ) {
