31a41c80051d6246fdb2842265c80a985d621f8e
Leighton <leighsapir@icloud.com>
2017-10-22 16:56:14 -0500

    [Trash] Remove unnecessary code

--- admin/class-hjconnect-admin.php
+++ admin/class-hjconnect-admin.php
@@ -78,29 +78,6 @@ class Hjconnect_Admin {
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Hjconnect_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Hjconnect_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/hjconnect-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
-
-	/**
 	 * Register the administration menu for this plugin into WordPress Dashboard menu.
 	 * 
 	 * @since 1.0.0
